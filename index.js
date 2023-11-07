8ec956786309ff01530b49dedf6ecf06136c7365
moe beydoun <moebeydoun24@yahoo.com>
2023-11-07 14:03:03 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName)
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavortieCustomer = 'bob'
+
+function changeLeastFavoriteCustomer() {
+    leastFavortieCustomer = 'jack'
+
+}
